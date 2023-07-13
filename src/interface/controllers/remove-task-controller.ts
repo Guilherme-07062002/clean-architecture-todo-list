95ed585adcee0e39e01de7bc58d3cd09ac166025
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-13 09:50:04 -0300

    remove task controller

---
+++ src/interface/controllers/remove-task-controller.ts
@@ -0,0 +1,22 @@
+import { removeTaskDTO } from "@/domain/dtos"
+import { Controller, Request, Response, UseCase } from "@/domain/ports"
+import { ok } from '../adapters';
+
+namespace Request {
+    export type Query = {
+        id: number
+    }
+}
+
+export class RemoveTaskController implements Controller {
+    constructor(
+        private readonly useCase: UseCase
+    ) { }
+
+    async handle(request: Request<unknown, Request.Query>): Promise<Response> {
+        const query = request.query
+        const response = await this.useCase.execute(query as removeTaskDTO)
+        
+        return ok(response)
+    }
+}
