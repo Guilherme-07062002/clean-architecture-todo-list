fd21e728970e769bb875e057e167b0e58c3e7579
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-14 09:12:42 -0300

    install mongoose

---
+++ src/infra/repositories/mongo/mongo-task-repository.ts
@@ -0,0 +1,11 @@
+import { TaskRepository } from "@/domain/repositories/task-repository";
+import mon
+
+export class MongoTaskRepository implements TaskRepository {
+    private db
+
+    constructor(
+
+    ) { }
+
+}
