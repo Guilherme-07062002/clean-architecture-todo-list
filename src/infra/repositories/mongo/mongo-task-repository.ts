395373aaa101491d54a1a65b175cd79e430f7748
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-14 09:51:52 -0300

    create function mongo repository

--- src/infra/repositories/mongo/mongo-task-repository.ts
+++ src/infra/repositories/mongo/mongo-task-repository.ts
@@ -1,11 +1,48 @@
+import { createTaskDTO, removeTaskDTO, updateTaskDTO } from "@/domain/dtos";
+import { Task } from "@/domain/entities";
 import { TaskRepository } from "@/domain/repositories/task-repository";
-import mon
+import mongoose, { Model } from "mongoose";
+
+interface TaskDocument extends mongoose.Document {
+  id?: number;
+  description: string;
+}
+
+const TaskSchema = new mongoose.Schema<TaskDocument>({
+  id: {
+    type: Number,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
 
 export class MongoTaskRepository implements TaskRepository {
-    private db
+  private taskModel: Model<TaskDocument>;
 
-    constructor(
+  constructor(url: string) {
+    // "mongodb://localhost/bdTask"
+    mongoose.connect(url);
 
-    ) { }
+    this.taskModel = mongoose.model<TaskDocument>("Task", TaskSchema);
+  }
 
+  async create(data: createTaskDTO): Promise<Task | Object> {
+    const task = await this.taskModel.create(data);
+    if (!task) return { status: false };
+    return { status: true };
+  }
+
+  async remove(data: removeTaskDTO): Promise<boolean> {
+    return true;
+  }
+
+  async update(data: updateTaskDTO): Promise<boolean> {
+    return true;
+  }
+
+  // async list(): Promise<Task[]> {
+  //     return Task[]
+  // }
 }
